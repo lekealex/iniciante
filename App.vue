a18f052d669f50085480c99e5be880044660f9f6
lekealex <106382948+lekealex@users.noreply.github.com>
2022-05-30 09:30:01 -0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <header>
+    <span class="texto-header">home</span>
+    <span class="separador"> </span>
+    <span class="texto-header"> jogadores</span>
+    <span class="separador"> </span>
+    <span class="texto-header">Times</span>
+  </header>
+  <main>main</main>
+  <footer>rodapé</footer>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  max-width: 1920px;
+  margin: 0 auto;
+  padding: 2rem;
+  background-color: aqua;
+  /* padding: 2rem; */
+  height: 100vh;
+
+  font-weight: normal;
+}
+
+header,
+footer {
+  background-color: blue;
+  height: 15%;
+  font-size: 1.3rem;
+  color: #fff;
+}
+
+main {
+  min-height: 70%;
+}
+
+header span {
+  padding: 0 10px;
+}
+</style>
